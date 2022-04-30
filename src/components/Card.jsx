3a4e6d2f1ae4e0a80c87067ab5d990159154d3d5
maliha991 <maliha.mithila24@gmail.com>
2022-05-01 03:10:45 +0600

    add carousel

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { AiOutlineHeart } from "react-icons/ai";
 
-import apartment from "../images/pexels-lilartsy-3016321.jpg";
+import apartment1 from "../images/apartment1.jpg";
+import apartment2 from "../images/apartment2.jpg";
+import apartment3 from "../images/apartment3.jpg";
+import apartment4 from "../images/apartment4.jpg";
+import apartment5 from "../images/apartment5.jpg";
 import bath from "../images/bath.png";
 import bed from "../images/bed (1) 1.png";
 import fullScreen from "../images/full-screen 1.png";
@@ -9,12 +13,35 @@ import fullScreen from "../images/full-screen 1.png";
 import "./Card.css";
 
 const Card = ({ status }) => {
+	const images = [apartment1, apartment2, apartment3, apartment4, apartment5];
+	const [activeImageIndex, setActiveImageIndex] = useState(0);
+	const [activeImage, setActiveImage] = useState(images[activeImageIndex]);
+
+	useEffect(() => {
+		setActiveImage(images[activeImageIndex]);
+	}, [activeImageIndex]);
+
+	setTimeout(
+		() => setActiveImageIndex((activeImageIndex + 1) % images.length),
+		3000
+	);
+
 	return (
 		<div className="card-container">
-			<img className="apartment" src={apartment} alt="apartment" />
+			<img className="apartment" src={activeImage} alt="apartment" />
+
 			<div className="heart-container">
 				<AiOutlineHeart className="heart" />
 			</div>
+
+			<div className="carousel">
+				{images.map((image, index) => (
+					<div
+						className={index === activeImageIndex ? "active-dot" : "dot"}
+					></div>
+				))}
+			</div>
+
 			<h2 className="address">5300 Lakeside, Newyork</h2>
 
 			<div className="buy-rate">
